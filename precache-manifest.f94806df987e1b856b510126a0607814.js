2be4f7342d1d01d5af6d7c5f3f2d15df804b0f12
cc6715 <cc6715@ic.ac.uk>
2020-02-11 23:35:54 +0000

    Updates

---
+++ precache-manifest.f94806df987e1b856b510126a0607814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffa9950d77ad94edf2b7100fee9b0cc",
+    "url": "/nextPage/index.html"
+  },
+  {
+    "revision": "078a5e9e386bd4895a15",
+    "url": "/nextPage/static/css/main.3dcdb9b8.chunk.css"
+  },
+  {
+    "revision": "982b01da77ad6c08ca22",
+    "url": "/nextPage/static/js/2.90f4f46a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nextPage/static/js/2.90f4f46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078a5e9e386bd4895a15",
+    "url": "/nextPage/static/js/main.4e3ec24e.chunk.js"
+  },
+  {
+    "revision": "c2d52f681b710b21f67a",
+    "url": "/nextPage/static/js/runtime-main.ca149ff6.js"
+  }
+]);
--- src/lib/fontawesome.jsx
+++
@@ -1,8 +0,0 @@
-// library
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-// icons
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, fas);
